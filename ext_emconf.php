9fc865160ef2ccb857c77902fadc9b0457e3d93c
Claus Due <claus@wildside.dk>
2012-12-09 10:39:03 +0100

    [TER] 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,11 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "view".
  *
+<<<<<<< HEAD
  * Auto generated 05-12-2012 02:35
+=======
+ * Auto generated 09-12-2012 10:37
+>>>>>>> [TER] 1.1.0
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +19,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Extends the built-in Fluid Views with smart overlay capabilities',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '1.0.0',
+	'version' => '1.1.0',
 	'dependencies' => 'cms,extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +47,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"8e4a";s:9:"README.md";s:4:"c012";s:46:"Classes/Override/View/ExtendedTemplateView.php";s:4:"740e";}',
+	'_md5_values_when_last_written' => 'a:4:{s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"c171";s:9:"README.md";s:4:"c012";s:46:"Classes/Override/View/ExtendedTemplateView.php";s:4:"6dbf";}',
 	'suggests' => array(
 	),
 );
