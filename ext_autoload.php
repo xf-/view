e193b7ef968a19f9b24577350900c2fbd5de5d2a
Claus Due <claus@wildside.dk>
2012-12-09 10:24:08 +0100

    [TASK] Autoloader for 4.5

---
+++ ext_autoload.php
@@ -0,0 +1,5 @@
+<?php
+	$extensionClassesPath = t3lib_extMgm::extPath('view') . 'Classes/';
+return array(
+	'tx_view_override_view_extendedtemplateview' => $extensionClassesPath . 'Override/View/ExtendedTemplateView.php',
+);
