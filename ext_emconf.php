c580101f66ccedbaf61f685f7b988107d189d59f
Claus Due <claus@wildside.dk>
2013-05-19 12:48:18 +0200

    [TER] 1.2.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "view".
  *
- * Auto generated 17-03-2013 23:36
+ * Auto generated 19-05-2013 12:48
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Extends the built-in Fluid Views with smart overlay capabilities',
 	'category' => 'misc',
 	'shy' => 0,
-	'version' => '1.2.0',
+	'version' => '1.2.1',
 	'dependencies' => 'cms,extbase,fluid',
 	'conflicts' => '',
 	'priority' => '',
@@ -43,7 +43,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:6:{s:16:"ext_autoload.php";s:4:"e0c9";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"1b28";s:9:"README.md";s:4:"384c";s:61:"Classes/Override/Parser/SyntaxTree/ExtendedViewHelperNode.php";s:4:"673c";s:46:"Classes/Override/View/ExtendedTemplateView.php";s:4:"c70f";}',
+	'_md5_values_when_last_written' => 'a:6:{s:16:"ext_autoload.php";s:4:"e0c9";s:12:"ext_icon.gif";s:4:"e922";s:17:"ext_localconf.php";s:4:"1b28";s:9:"README.md";s:4:"384c";s:61:"Classes/Override/Parser/SyntaxTree/ExtendedViewHelperNode.php";s:4:"c031";s:46:"Classes/Override/View/ExtendedTemplateView.php";s:4:"4ecf";}',
 	'suggests' => array(
 	),
 );
